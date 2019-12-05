da539b513d6fc4d09b07b1d077022060e998cc33
Dmitriy Nikolaev <dmnikolaev@outlook.com>
2019-12-05 12:16:05 +0500

    t2

--- ftdicomm/MainWindow.xaml.cs
+++ ftdicomm/MainWindow.xaml.cs
@@ -20,9 +20,33 @@ namespace ftdicomm
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string deviceSerial;
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadShowProperties();
+            Run();
+        }
+
+        private void LoadShowProperties()
+        {
+            this.deviceSerial = Properties.Settings.Default.devSerial;
+            this.Top = Properties.Settings.Default.x;
+            this.Left = Properties.Settings.Default.y;
+            tbPropList.Text = $"{this.deviceSerial}\n{this.Top.ToString()}\n{this.Left.ToString()}";
+        }
+        
+        private void Run()
+        {
+            
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.x  = this.Top;
+            Properties.Settings.Default.y = this.Left;
+            Properties.Settings.Default.Save();
         }
     }
 }
